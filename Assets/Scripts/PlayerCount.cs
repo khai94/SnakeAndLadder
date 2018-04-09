37a461a2a40acb0d6ac991fcf152d5c7e72e2b81
Khai <khaireal@gmail.com>
2018-04-10 00:01:03 +0800

    player slots
    added the ability to change the number of player slots in the game lobby

---
+++ Assets/Scripts/PlayerCount.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerCount : MonoBehaviour {
+
+	public Dropdown dropdown;
+	public GameObject[] playerArray;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SetPlayerCount()
+	{
+		int count = dropdown.value + 2;
+
+		foreach (GameObject go in playerArray) {
+			go.SetActive (false);
+		}
+
+		for (int i = 0; i < count; i++) {
+			playerArray [i].SetActive (true);
+		}
+	}
+}
