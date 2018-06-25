dbaaac3d93a4c3d5891db1803d4c3bc1293ca85c
Voltz <35767373+V0lteX@users.noreply.github.com>
2018-06-26 01:26:16 +0800

    Added various card's scripts
    non-tested/debugged, might not work well but requires a lots of changes and reviews for further improvement so far...

---
+++ Assets/Scripts/ElevatorCard.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Effect
+{
+	upward
+}
+
+public class StatusSystem : MonoBehaviour {
+	public string name;
+	public string description;
+	public Effect effect;
+
+	public void ExecuteEffect(int i)
+	{
+		effect = (Effect) i;
+
+		switch (effect) {
+		case Effect.upward:
+			Debug.Log ("Moving Upwards");
+			break;
+		}
+	}
+}
---
+++ Assets/Scripts/ParalyzeCard.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Effect
+{
+	stun
+}
+
+public class StatusSystem : MonoBehaviour {
+	public string name;
+	public string description;
+	public Effect effect;
+
+	public void ExecuteEffect(int i)
+	{
+		effect = (Effect) i;
+
+		switch (effect) {
+		case Effect.stun:
+			Debug.Log ("Stunned");
+			break;
+		}
+	}
+}
