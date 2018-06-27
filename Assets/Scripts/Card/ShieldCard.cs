391f4626eaee3f695855e5d4cb93a3602682c19f
Voltz <35767373+V0lteX@users.noreply.github.com>
2018-06-27 23:48:08 +0800

    Added new Card's Scripts
    The newer card's scripts has change from Monobehavior to ScriptableObject with larger modularity and reduce complexity of the tyranny Monobehavior....
    Presumably future changes of each Cards would use the same methods if possible.

---
+++ Assets/Scripts/Card/ShieldCard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+public class Card : ScriptableObject {
+
+	public new string name;
+	public string description;
+
+	public Sprite artwork;
+
+	public int turnCost;
+	public int invincible;
+	public int selfBuff;
+
+	public void Print ()
+	{
+		Debug.Log (name + ": " + description + "The card costs: " + turnCost);
+	}
+}
