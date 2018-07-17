f5d4abea5f7f5d8e77d2cc02e0767f74db3b68ac
Voltz <35767373+V0lteX@users.noreply.github.com>
2018-07-18 00:59:18 +0800

    Added new Card's scripts
    New card's scripts that have yet to have its own individual component and requires sprite's artwork assets.

---
+++ Assets/Scripts/Card/PlusTwoCard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlusTwoCard : ScriptableObject
+{
+
+    public new string name;
+    public string description;
+
+    public Sprite artwork;
+
+    public int turnCost;
+    public int moveForward;
+
+    public void Print()
+    {
+        Debug.Log(name + ": " + description + " The card costs: " + turnCost);
+    }
+
+}
