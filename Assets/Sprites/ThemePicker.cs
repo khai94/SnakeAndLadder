7efc4ecb487d7ca09d81a8f5d7d8197984a40234
Khai <khaireal@gmail.com>
2018-08-14 14:08:02 +0800

    transfer PC commit
    transfering data to new PC

---
+++ Assets/Sprites/ThemePicker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ThemePicker : MonoBehaviour {
+    Theme theme;
+    Image image;
+
+	// Use this for initialization
+	void Start () {
+        theme = GameObject.Find("ThemeManager").GetComponent<Theme>();
+        image = gameObject.GetComponent<Image>();
+
+        image.color = theme.themeColor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
